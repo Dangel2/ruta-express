585115a2da76cea61248965855ef63282314c3cd
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 17:42:38 -0600

    Create Promotions.jsx

---
+++ frontend/src/components/Promotions.jsx
@@ -0,0 +1,23 @@
+function Promotions() {
+  return (
+    <section id="promociones">
+
+      <h2>Promociones</h2>
+
+      <div className="promo-card">
+         Tu 4.º viaje de la semana es GRATIS
+      </div>
+
+      <div className="promo-card">
+         Tarifas especiales para negocios
+      </div>
+
+      <div className="promo-card">
+         Prioridad para clientes frecuentes
+      </div>
+
+    </section>
+  );
+}
+
+export default Promotions;
