6869aa9df5708e2c556d35befa897d3d8a2daeeb
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 17:40:33 -0600

    Create Services.jsx

---
+++ frontend/src/components/Services.jsx
@@ -0,0 +1,39 @@
+function Services() {
+  return (
+    <section id="servicios">
+
+      <h2>Servicios</h2>
+
+      <div className="grid">
+
+        <div className="card">
+           Compras en supermercados
+        </div>
+
+        <div className="card">
+           Compras en farmacias
+        </div>
+
+        <div className="card">
+           Entrega de paquetes
+        </div>
+
+        <div className="card">
+           Pago de servicios
+        </div>
+
+        <div className="card">
+           Trámites sencillos
+        </div>
+
+        <div className="card">
+           Mandados personales
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Services;
