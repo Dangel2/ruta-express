60415ff344df097cd91d05b276f422eeef221759
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 17:39:44 -0600

    Create Hero.jsx

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+function Hero() {
+  return (
+    <section className="hero">
+
+      <h1>
+        Delivery y Mandados
+        en Carazo y Nicaragua
+      </h1>
+
+      <p>
+        Rápido • Confiable • Seguro
+      </p>
+
+      <p>
+        Tú lo pides, yo voy por ti.
+      </p>
+
+      <a
+        href="https://wa.me/50586109523"
+        className="hero-btn"
+      >
+        Solicitar Servicio
+      </a>
+
+    </section>
+  );
+}
+
+export default Hero;
