1dc26c0de63e00b9dd558a8b674539eb45a52ab4
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 18:31:01 -0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,20 @@
-console.log('API Ruta Express');
+import express from "express";
+import cors from "cors";
+
+import healthRoutes from "./routes/health.routes.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/health", healthRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Servidor corriendo en puerto ${PORT}`
+  );
+});
