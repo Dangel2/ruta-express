306bd6122f2fea9d6be3bc8e96fb70b5e96aa147
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 18:09:53 -0600

    Create PromoBanner.jsx

---
+++ frontend/src/components/PromoBanner.jsx
@@ -0,0 +1,17 @@
+function PromoBanner() {
+  return (
+    <section className="promo-banner">
+
+      <h2>
+         ¡Tu 4.º viaje de la semana es GRATIS!
+      </h2>
+
+      <p>
+        Aprovecha nuestras promociones para clientes frecuentes.
+      </p>
+
+    </section>
+  );
+}
+
+export default PromoBanner;
