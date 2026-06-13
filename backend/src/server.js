3714025294fc3b02588a8e610f4fd46bc33dc3e2
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-12 19:49:27 -0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import healthRoutes from "./routes/health.routes.js";
+import { initDatabase } from "./config/initDb.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 
+// Inicializar tablas
+await initDatabase();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Servidor corriendo en puerto ${PORT}`
-  );
+  console.log(` Servidor corriendo en puerto ${PORT}`);
 });
